ffd3de4773b837ed6066293b5f3b8093e57f1e77
akshay malviya <102719327+akshay-malviya12@users.noreply.github.com>
2024-02-13 19:09:39 +0530

    Update login.js
    Update login page and add Malti Factor Authentication.

--- login.js
+++ login.js
@@ -2,45 +2,77 @@ const express = require("express");
 const db = require("./database");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+// const nodeeamiler = require("nodemailer");
+// const speakeasy = require("speakeasy");
+//for both receive
+// const {sendEmailForVerify, generateRandomcodeforotp        }= require("./nodemailer");
+const { sendEmailForVerify } = require("./nodemailer");
+const { generateRandomcodeforotp } = require("./generateRandom");
 
 // Secret key for JWT
 const secretKey = "secretkey";
 
 // Login route with multi-factor authentication
 router.post("/", (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  const query = "SELECT * FROM usersAuths WHERE email = ? AND password = ?";
-  db.get(query, [email, password], (err, user) => {
+  const { email, password, verificationcode } = req.body;
+  console.log(email, password, verificationcode);
+  console.log("email", email);
+  const query =
+    "SELECT usersAuths.*,OTPVarifications.* FROM usersAuths INNER JOIN OTPVarifications ON usersAuths.email=OTPVarifications.email WHERE usersAuths.email = ? ";
+
+  db.get(query, [email], (err, user) => {
     if (err) {
+      console.log("errorsssss", err);
       return res.status(500).json({ message: "Internal server error" });
     }
     if (!user) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(401).json({ message: "Invalid email " });
     }
-    // Perform MFA here, send verification code to email/phone and verify it
-    const token = jwt.sign(
-      { id: user.name.length, email: user.email, role: user.type },
-      secretKey,
-      {
-        expiresIn: "1h",
-      },
-    );
+    //check if the user is verification code is correct.
+    if (user.verificationcode !== verificationcode) {
+      return res.status(401).json({ message: "Invalid verification code" });
+    }
 
-    db.run(
-      "UPDATE usersAuths SET token = ? WHERE email = ?",
-      [token, email],
-      (err) => {
-        if (err) {
-          return res.status(500).json({ message: "Internal1 server error" });
-        }
-        console.log("usertoken", user);
-        res.status(200).json({ message: "Login successful", token });
-      },
-    );
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) {
+        // Handle error
+        console.error(err);
+      } else if (result) {
+        // Passwords match
+
+        // Perform MFA here, send verification code to email/phone and verify it
+        const token = jwt.sign(
+          { id: user.name.length, email: user.email, role: user.type },
+          secretKey,
+          {
+            expiresIn: "1h",
+          },
+        );
+
+        db.run(
+          "UPDATE usersAuths SET token = ? WHERE email = ?",
+          [token, email],
+          (err) => {
+            if (err) {
+              return res
+                .status(500)
+                .json({ message: "Internal1 server error" });
+            }
+            console.log("usertoken", user);
+            res.status(200).json({ message: "Login successful", token });
+          },
+        );
+      } else {
+        // Passwords don't match
+
+        res.status(500).json({ message: "Login failed" });
+      }
+    });
 
     //  res.json({ token: token, user: user });
   });
 });
 
 module.exports = router;
+
